fe78de7389bec57ba833647d705cfab47a243c48
root <root@DESKTOP-K86DO2P.localdomain>
2019-03-23 22:14:59 +0800

    UI介面 & Fade測試

---
+++ GameProject/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+		public void GoChapterScene()
+		{
+			SceneManager.LoadScene("Chapter");
+		}
+
+		public void GoAnimationScene()
+		{
+			SceneManager.LoadScene("Animation");
+		}
+}
