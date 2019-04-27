a2c22beae74356d5ef79ace0cc042f72135f8dde
hellen6654 <hellen6654@gmail.com>
2019-04-27 23:16:54 +0800

    主選單功能進度87% 缺美術、遊戲動畫頁面、設定畫面>亮度調整  更改canvas架構 mainmeun移動未加 碰撞未加 controller 、sdkmanager已拉成prefab 但是之後應該還會加東西 音量大小有做可是沒有測試 音效被我藏在垃圾桶 bgm在canvas裡 大guy是這樣

--- GameProject/Assets/Scripts/Setting.cs
+++ GameProject/Assets/Scripts/Setting.cs
@@ -1,21 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Setting: MonoBehaviour {
     static public float BGMVolume = 0.5f;       //BGM音量預設50%
     static public float EffectVolume = 0.5f;    //音效音量預設50%
     static public float Gramma = 0.5f;          //亮度預設50%
-
     private AudioSource BGMAudioSource;
     private GameObject[] effectAudioSource;
     private GameObject[] grammaLight;
     public void BGMVolumeChanged(float v)
     {
-        BGMAudioSource = GameObject.FindGameObjectWithTag("BGM").GetComponent<AudioSource>(); ;
+        BGMAudioSource = GameObject.FindGameObjectWithTag("BGM").GetComponent<AudioSource>(); 
         Debug.Log("BGMVolume:"+v.ToString());
         BGMVolume = v;
-        BGMAudioSource.volume = v;
+        BGMAudioSource.volume = v; //把BGM的volume都改成v
     }
     public void EffectVolumeChanged(float v)
     {
@@ -28,11 +27,16 @@ public class Setting: MonoBehaviour {
     public void GrammaChanged(float v)
     {
         Debug.Log("Gramma:" + v.ToString());
-        Gramma = v;
-        grammaLight = GameObject.FindGameObjectsWithTag("Light");
-        foreach (var item in grammaLight)
-        {
-            item.GetComponent<Light>().intensity = v; //把所有燈光的intensity都改成v
-        }
+        Gramma = v ;
+        
+    }
+    
+    public void ExitGame()
+    {
+        #if UNITY_EDITOR // 如果是編輯模式 退出遊玩
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else //如果是遊玩模式 關閉程式
+            Application.Quit();
+        #endif
     }
 }
