cebb0f6a3e5c4558a7e37593780d214fc90ffa7e
hellen6654 <hellen6654@gmail.com>
2019-05-02 02:19:07 +0800

    新增clicked、hovered音效 mainmenuInit script已加 移動已加 setting按鈕hover、clicked有聲音

---
+++ GameProject/Assets/Scripts/EffectSound.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectSound : MonoBehaviour {
+    public AudioSource audioSource;
+    public AudioClip hover;
+    public AudioClip clicked;
+    public void HoveredSound()
+    {
+        audioSource.PlayOneShot(hover);
+    }
+    public void Clickedsound()
+    {
+        audioSource.PlayOneShot(clicked);
+    }
+}
---
+++ GameProject/Assets/Scripts/MainmenuInit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainmenuInit : MonoBehaviour {
+    private AudioSource BGMAudioSource;
+    private GameObject[] effectAudioSource;
+    // Use this for initialization
+    void Start () {
+        BGMAudioSource = GameObject.FindGameObjectWithTag("BGM").GetComponent<AudioSource>();
+        effectAudioSource = GameObject.FindGameObjectsWithTag("Effect");
+        BGMAudioSource.volume = Setting.BGMVolume;
+        foreach (var e in effectAudioSource)
+            e.GetComponent<AudioSource>().volume = Setting.EffectVolume;
+    }
+	
+}
