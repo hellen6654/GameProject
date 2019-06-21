bdfe11f5c71a63e48750bde4cb9323bc2e9f7a5e
hellen6654 <hellen6654@gmail.com>
2019-06-21 16:24:47 +0800

    頭盔碰撞手把震動 + Chapter 只有右手可以觸發書本

---
+++ GameProject/Assets/Scripts/HeadsetCollisionRumble.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+public class HeadsetCollisionRumble : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+		GetComponent<VRTK_HeadsetCollision>().HeadsetCollisionDetect += new HeadsetCollisionEventHandler(HeadsetCollisionControllerRumble);
+	}
+
+    private void HeadsetCollisionControllerRumble(object sender, HeadsetCollisionEventArgs e)
+    {
+        VRTK_ControllerHaptics.TriggerHapticPulse(VRTK_ControllerReference.GetControllerReference(SDK_BaseController.ControllerHand.Left),1);
+        VRTK_ControllerHaptics.TriggerHapticPulse(VRTK_ControllerReference.GetControllerReference(SDK_BaseController.ControllerHand.Right),1);
+        Debug.Log("ControllerRumble!!!");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
