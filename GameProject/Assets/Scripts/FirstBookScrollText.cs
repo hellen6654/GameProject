19e8cc5c89968c0b7da96fa1d5c8b4ca86dd9cd8
hellen6654 <hellen6654@gmail.com>
2019-09-06 01:42:01 +0800

    firstbook text book scroll

---
+++ GameProject/Assets/Scripts/FirstBookScrollText.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+public class FirstBookScrollText : MonoBehaviour {
+    // Use this for initialization
+    public float speed = 2; //速度
+    public float goalLen = 100; //最終位置
+    private float moveLen = 0;
+    void Start () {
+        
+    }
+    
+    // Update is called once per frame
+    void Update () {
+       
+        Move();
+	}
+    void Move()
+    {
+        //物體向前移動
+        Debug.Log(moveLen);
+        if (moveLen <= goalLen)
+        {
+            moveLen += Time.deltaTime * speed;
+            GetComponent<RectTransform>().Translate(0, Time.deltaTime * speed, 0);
+        }
+    }
+}
